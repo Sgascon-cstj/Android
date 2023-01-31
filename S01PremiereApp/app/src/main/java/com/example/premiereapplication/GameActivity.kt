9841d64f5cbbd20b7cdcc698598a766bef687b3e
Samuel <2151866@cstj.qc.ca>
2023-01-31 14:09:55 -0500

    Fin cours 3

--- S01PremiereApp/app/src/main/java/com/example/premiereapplication/GameActivity.kt
+++ S01PremiereApp/app/src/main/java/com/example/premiereapplication/GameActivity.kt
@@ -4,16 +4,77 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import org.w3c.dom.Text
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
+    private var nbTries = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        val nbrPicker = findViewById<NumberPicker>(R.id.nbrPicker)
+        val txvMessage = findViewById<TextView>(R.id.txtPlayerName)
+        val btnValidate = findViewById<Button>(R.id.btnCheck)
+        val txvTries = findViewById<TextView>(R.id.txvMessage)
+
+        val theGoodNumber = (0 .. 100).random()
+
+        txvMessage.text = getString(R.string.msgGame, intent.getStringExtra(PLAYER_NAME_EXTRA))
+        //Logic
+        nbrPicker.minValue = 0
+        nbrPicker.maxValue = 100
+
+        btnValidate.setOnClickListener {
+            nbTries ++
+ /*           if (nbrPicker.value == theGoodNumber)
+            {
+                Snackbar.make(btnValidate, getString(R.string.msgWin,nbTries.toString()), Snackbar.LENGTH_LONG).show()
+                txvTries.text = getString(R.string.msgWin,nbTries.toString())
+            }else if (nbrPicker.value > theGoodNumber)
+            {
+                Snackbar.make(btnValidate, "Le nombre recherché est plus petit", Snackbar.LENGTH_LONG).show()
+                txvTries.text = applicationContext.resources.getQuantityString(R.plurals.msgTries,nbTries,nbTries)
+            }else
+            {
+                Snackbar.make(btnValidate, "Le nombre recherché est plus grand", Snackbar.LENGTH_LONG).show()
+                txvTries.text = applicationContext.resources.getQuantityString(R.plurals.msgTries,nbTries,nbTries)
+
+            }
+*/
+            when{
+                nbrPicker.value == theGoodNumber ->{
+                    Snackbar.make(btnValidate, getString(R.string.msgWin,nbTries.toString()), Snackbar.LENGTH_LONG).show()
+                    txvTries.text = getString(R.string.msgWin,nbTries.toString())
+                    finish()
+                }
+                nbrPicker.value > theGoodNumber ->{
+                    Snackbar.make(btnValidate, "Le nombre recherché est plus petit", Snackbar.LENGTH_LONG).show()
+                    txvTries.text = applicationContext.resources.getQuantityString(R.plurals.msgTries,nbTries,nbTries)
+                }
+                nbrPicker.value < theGoodNumber ->{
+                    Snackbar.make(btnValidate, "Le nombre recherché est plus grand", Snackbar.LENGTH_LONG).show()
+                    txvTries.text = applicationContext.resources.getQuantityString(R.plurals.msgTries,nbTries,nbTries)
+                }
+             }
+        }
     }
+    //La partie statique de notre classe
     companion object {
-        fun newIntent(context: Context): Intent {
+         const val PLAYER_NAME_EXTRA = "PLAYER_NAME"
+        fun newIntent(context: Context, playerName: String): Intent {
             val intent = Intent(context,GameActivity::class.java)
+            //Dictionary string, object
+            intent.putExtra(PLAYER_NAME_EXTRA, playerName)
+
             return intent
         }
+
     }
 }
--- S01PremiereApp/app/src/main/java/com/example/premiereapplication/MainActivity.kt
+++ S01PremiereApp/app/src/main/java/com/example/premiereapplication/MainActivity.kt
@@ -5,9 +5,9 @@ import android.graphics.Color
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Telephony.Sms
+import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import android.widget.EditText
 import kotlin.random.Random
 
 
@@ -17,21 +17,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnPhone:Button = findViewById<Button>(R.id.btnPhone)
+        val btnSMS = findViewById<Button>(R.id.btnSMS)
+        val btnGame = findViewById<Button>(R.id.btnGame)
+        val edtPlayerName = findViewById<EditText>(R.id.edtPlayerName)
+
+        //btn phone
         btnPhone.setOnClickListener {
             it.setBackgroundColor(Color.rgb(Random.nextInt(256),Random.nextInt(256),Random.nextInt(256)))
             //Toast.makeText(this,R.string.msgHello,Toast.LENGTH_LONG).show()
             val intent = Intent(Intent.ACTION_DIAL,Uri.parse("tel:4505670123"))
             startActivity(intent)
         }
-        val btnSMS = findViewById<Button>(R.id.btnSMS)
+        //btn sms
         btnSMS.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("smsto:4504361580"))
             intent.putExtra("sms_body","Message de mon code")
             startActivity(intent)
         }
-        val btnGame = findViewById<Button>(R.id.btnGame)
+        //btn game
         btnGame.setOnClickListener {
-            startActivity(GameActivity.newIntent((this)))
+            startActivity(GameActivity.newIntent(this,edtPlayerName.text.toString()))
         }
+
+
+
     }
 }
